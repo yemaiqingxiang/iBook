12bf8d6c4fe425efba90a12c28b534969a564808
yemaiqingxiang <yemaiqingxiang@163.com>
2018-11-19 17:32:38 +0800

    123

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,9 +10,9 @@ public class Main {
     private static final String bookName = "Vue";
     public static void main(String[] args) {
         //更新目录
-//        new Book(bookName);
+        new Book(bookName);
         //创建书文件夹
-        new Add(bookName, ".png");
+//        new Add(bookName, ".png");
     }
 
 }
