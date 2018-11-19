5dc497db75868b794e32eb21aa0366e14526f65c
yemaiqingxiang <yemaiqingxiang@163.com>
2018-11-19 17:26:28 +0800

    123

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,12 +7,12 @@ import com.company.add.Book;
 public class Main {
 //    private static final String bookName = "Spring-mvc";
 //    private static final String bookName = "DD";
-    private static final String bookName = "Activiti";
+    private static final String bookName = "Vue";
     public static void main(String[] args) {
         //更新目录
-        new Book(bookName);
+//        new Book(bookName);
         //创建书文件夹
-//        new Add(bookName, ".png");
+        new Add(bookName, ".png");
     }
 
 }
