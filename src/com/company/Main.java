f14b29de06693b7add8e8ff56d03882cbf66f16b
wanggc <yemaiqingxiang@163.com>
2019-01-23 09:58:54 +0800

    123

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,9 @@ import com.company.add.Add;
 import com.company.add.Book;
 
 public class Main {
-//    private static final String bookName = "Spring-mvc";
+    private static final String bookName = "Spring-mvc";
 //    private static final String bookName = "DD";
-    private static final String bookName = "Vue";
+//    private static final String bookName = "Vue";
     public static void main(String[] args) {
         //更新目录
         new Book(bookName);
